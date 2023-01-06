3b9235552ef9402354bdc783e7d3c63b9ac0563b
Emir <40762681+emirgo@users.noreply.github.com>
2023-01-06 19:35:04 +0100

    scanning

--- Entry.cpp
+++ Entry.cpp
@@ -6,15 +6,18 @@
 
 int main(void)
 {
-	HANDLE snapshot = 0;
+	HANDLE snapshot, process = 0;
 	PROCESSENTRY32 process_entry = { 0 };
 
 	process_entry.dwSize = sizeof(PROCESSENTRY32);
 	snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 	Process32First(snapshot, &process_entry);
 
+	std::cout << "scanning";
 	do {
+		std::cout << ".";
 		if (wcscmp(process_entry.szExeFile, L"ac_client.exe") == 0) {
+			std::cout << std::endl;
 			std::cout << "found" << std::endl;
 		}
 	} while (Process32Next(snapshot, &process_entry));
