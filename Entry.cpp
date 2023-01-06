114e152bc16ea3dffd07c027b91359e1c34d7b08
emirgo <emirgoppa@gmail.com>
2023-01-06 15:48:27 +0100

    thread

--- Entry.cpp
+++ Entry.cpp
@@ -1,7 +1,18 @@
 #include <Windows.h>
 
-BOOL WINAPI DllMain(HINSTANCE hinst_dll, DWORD fdw_reason, LPVOID lpv_reserved) {
-	MessageBox(0, 0, 0, 0);
+void Hook() 
+{
+	while (true) {
+		Sleep(1);
+	}
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinst_dll, DWORD fdw_reason, LPVOID lpv_reserved) 
+{
+	if (fdw_reason == DLL_PROCESS_ATTACH) {
+		MessageBox(0, 0, 0, 0);
+		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Hook, NULL, 0, NULL);
+	}
 
 	return true;
 }
