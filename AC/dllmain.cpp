c91173a3911890d4da39f6e0f941811c31e5bff7
Emir <40762681+emirgo@users.noreply.github.com>
2023-01-07 19:20:43 +0100

    hook exposure

--- AC/dllmain.cpp
+++ AC/dllmain.cpp
@@ -2,12 +2,35 @@
 #include "Offset.h"
 #include "Entity.h"
 
+std::uintptr_t hook_addr = 0x0040AD9D; // _asm mov edi, eax
+
+DWORD ori_call_addr = 0x4607C0;
+DWORD ori_jump_addr = 0x0040AA2;
+
+INPUT input = { 0 };
+DWORD edi_value = 0;
+
+__declspec(naked) void codecave()
+{
+    // call trigger hook and save all the registers
+    // finally expose eax by returning
+    __asm {
+        call ori_cal_addr
+        pushad
+        mov edi_value, eax
+    }
+}
+
 void Threaded()
 {
     std::uintptr_t module_base = 0x00;
     entity* local_player = (entity*)(module_base + Offset::local_entity_base);
 
+    DWORD old_protect = 0;
     while (true) {
+        VirtualProtect((void*)hook_addr, 5, PAGE_EXECUTE_READWRITE, &old_protect);
+        hook_addr = 0xE9;
+        *(DWORD*)(hook_addr + 1) = (DWORD)&codecave - ((DWORD)hook_addr + 5);
         Sleep(1);
     }
 }
