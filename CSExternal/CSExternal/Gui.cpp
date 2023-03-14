9ee5268cabef66324fc1a233ca8ad4f06c6d70d3
emirgo <emirgoppa@gmail.com>
2023-03-14 22:29:37 +0100

    gui cpp prototype

---
+++ CSExternal/CSExternal/Gui.cpp
@@ -0,0 +1,42 @@
+#include "Gui.h"
+
+void gui::CreateHWindow(const char* window_name, const char* class_name) noexcept
+{
+}
+
+void gui::DestroyHWWindow() noexcept
+{
+}
+
+bool gui::CreateDevice() noexcept
+{
+	return false;
+}
+
+void gui::ResetDevice() noexcept
+{
+}
+
+void gui::DestroyDevice() noexcept
+{
+}
+
+void gui::CreateImGui() noexcept
+{
+}
+
+void gui::DestroyImGui() noexcept
+{
+}
+
+void gui::BeginRender() noexcept
+{
+}
+
+void gui::EndRender() noexcept
+{
+}
+
+void gui::Render() noexcept
+{
+}
